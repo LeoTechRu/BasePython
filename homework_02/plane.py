af837082615893bfdbf3267d73de78c9750a6a4d
Leonid Kokorev <leotechru@gmail.com>
2025-08-30 16:35:36 +0300

    Implement vehicle hierarchy and mechanics

--- homework_02/plane.py
+++ homework_02/plane.py
@@ -1,3 +1,22 @@
-"""
-создайте класс `Plane`, наследник `Vehicle`
-"""
+"""Plane module with :class:`Plane` inheriting :class:`Vehicle`."""
+
+from .base import Vehicle
+from .exceptions import CargoOverload
+
+
+class Plane(Vehicle):
+    def __init__(self, weight: int, fuel: float, fuel_consumption: float, max_cargo: int) -> None:
+        super().__init__(weight, fuel, fuel_consumption)
+        self.max_cargo = max_cargo
+        self.cargo = 0
+
+    def load_cargo(self, cargo: int) -> None:
+        if self.cargo + cargo > self.max_cargo:
+            raise CargoOverload()
+        self.cargo += cargo
+
+    def remove_all_cargo(self) -> int:
+        cargo = self.cargo
+        self.cargo = 0
+        return cargo
+
