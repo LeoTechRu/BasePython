ff0308b19c4a0cf15d52bb45de0d3b0dd03af613
Leonid Kokorev <leotechru@gmail.com>
2025-08-30 18:32:27 +0300

    Replace deprecated yield_fixture

--- testing/conftest.py
+++ testing/conftest.py
@@ -8,7 +8,7 @@ helpers = Path(__file__).resolve().parent / "helpers"
 sys.path.insert(0, str(helpers))
 
 
-@pytest.yield_fixture(scope="session")
+@pytest.fixture(scope="session")
 def event_loop():
     """Create an instance of the default event loop for each test case."""
     loop = asyncio.get_event_loop_policy().new_event_loop()
