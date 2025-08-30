cfcde992bfddc6ecc173f1d172b3b68d6ff6e23e
Leonid Kokorev <leotechru@gmail.com>
2025-08-30 17:51:02 +0300

    Add generic class-based views for blog posts

---
+++ homework_08/posts/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Post(models.Model):
+    """A blog post."""
+
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:  # pragma: no cover - simple representation
+        return self.title
